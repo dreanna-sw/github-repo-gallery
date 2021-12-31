ed0e284039d11e467f5f034a9efccc9ea9a5a049
dreanna-sw <85309671+dreanna-sw@users.noreply.github.com>
2021-12-31 12:55:37 -0500

    Fetch Repo Data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Profile Info //
 const overview = document.querySelector(".overview");
 const username = "dreanna-sw";
+const repoList = document.querySelector(".repo-list");
 
 const getProfile = async function () {
     const profile = await fetch(`https://api.github.com/users/${username}`);
@@ -24,5 +25,21 @@ const displayUserInfo = function (data) {
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div> `;
   overview.append(divElement);
+  getRepos();
 };
 
+const getRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    console.log(repoData);
+    repoInfo(repoData);
+};
+
+const repoInfo = function (repos) {
+    for (const repo of repos) {
+        const listItem = document.createElement("li");
+        listItem.classList.add("repo");
+        listItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(listItem);
+    }
+};
